6125b388f87673eb328d8cc23f6172ade757fdf8
Kamal Nasser <hello@kamal.io>
2014-07-21 18:46:31 +0300

    Allow custom options to be passed to request()

--- test/test.js
+++ test/test.js
@@ -15,5 +15,15 @@ describe('expand-url', function(){
 			done();
       });
     });
+
+    // test custom options
+    it('Expand http://goo.gl/siv9G7', function(done){
+      urlExpander.expand("http://google.com", {
+            url: "http://goo.gl/siv9G7"
+      }, function(err, longUrl){
+			longUrl.should.equal("http://timesmachine.nytimes.com/timesmachine/1970/03/03/78101709.html");
+			done();
+      });
+    });
   });
 });
